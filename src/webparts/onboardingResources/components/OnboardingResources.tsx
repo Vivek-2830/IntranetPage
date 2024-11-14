239d803e3823f0920bebf44e98c8745480c5573e
Vivek-2830 <gohilvivek0303@gmail.com>
2024-11-14 14:23:01 +0530

    Complete Project

--- src/webparts/onboardingResources/components/OnboardingResources.tsx
+++ src/webparts/onboardingResources/components/OnboardingResources.tsx
@@ -36,6 +36,31 @@ export default class OnboardingResources extends React.Component<IOnboardingReso
           <div className='ms-Grid'>
             <div className='ms-Grid-row'>
               <div className='ms-Grid-col'>
+                <div className='Onboarding'>
+                  <h2>ONBOARDING RESOURCES</h2>
+                    <div className='Onboarding-Text'> 
+                      <div className='Resources-Items'>
+                        <img src="https://www.nestleprofessional.com.au/sites/default/files/2022-05/nestle-brands-beverages.jpg" alt="Meet New Hires"/>
+                        <p>MEET NEW HIRES</p>
+                      </div>
+
+                      <div className='Resources-Items'>
+                        <img src="https://trajanwealth.com/wp-content/uploads/2023/08/series-of-doors.jpg" alt="Meet New Hires"/>
+                        <p>CAREERS</p>
+                      </div>
+
+                      <div className='Resources-Items'>
+                        <img src="https://minoritynurse.com/wp-content/uploads/2020/09/Online-Image-1.jpg" alt="Meet New Hires"/>
+                        <p>LEARNING</p>
+                      </div>
+                    </div>
+                </div>
+
+                <footer className="footer">
+                    <div className='footer-text'>Copyright (c) 2024 ORIGAMI</div>
+                    <div className='contact-text'>Contact: vivekg1082004@outlook.com</div>
+                </footer>
+
               </div>
             </div>
           </div>
